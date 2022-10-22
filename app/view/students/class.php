2146235f147ce8fdc7ccd1e5a72edfd6b961a58e
XEZO3 <75046635+XEZO3@users.noreply.github.com>
2022-10-23 00:07:38 +0300

    style fixes

--- app/view/students/class.php
+++ app/view/students/class.php
@@ -1,20 +1,36 @@
 
 <!-- <a href="<?=PATH?>meeting/joinmeeting/<?=$id?>" >join now</a> -->
 
-
-<div class="container">
-    <div class="row justify-content-center">
+<style>
+    
+</style>
+<div class="container mt-5">
+<div class="row justify-content-center">
         <div class="col col-md-auto d-flex justify-content-center">
-        <a href="<?=PATH?>student/join/<?=$id?>">
-        <div class="card" style="width: 18rem;">
-        <img src="<?=PATH?>micons/live-streaming.png" alt="notfound" class="card-img-top" style="width:100%;height:20em" /> 
-        </div> 
-        </a>
+            <a href="<?=PATH?>student/join/<?=$id?>">
+                    <div  class="card" style="width: 18rem;height:18rem;background: linear-gradient(to right, #cc0000 28%, #ff99cc 122%);position:relative">
+                    <span style="position: absolute;top:44%;left:30%;font-size:30px;color:white">Live Class</span>
+                    </div> 
+            </a>
         </div>
         <div class="col col-md-auto d-flex justify-content-center">
             <a href="<?=PATH?>student/recordShow/<?=$id?>">
-            <div class="card" style="width: 18rem;">
-            <img src="<?=PATH?>micons/video.png" alt="notfound" class="card-img-top" style="width:100%;height:20em" /> 
+            <div  class="card" style="width: 18rem;height:18rem;background:linear-gradient(to right, #3366ff 18%, #66ccff 67%);">
+            <span style="position: absolute;top:44%;left:34%;font-size:30px;color:white">records</span>
+            </div> 
+            </a>
+        </div>
+        <div class="col col-md-auto d-flex justify-content-center">
+            <a href="<?=PATH?>student/recordShow/<?=$id?>">
+            <div  class="card" style="width: 18rem;height:18rem;background:linear-gradient(to right, #339933 12%, #00cc99 70%)">
+            <span style="position: absolute;top:44%;left:35%;font-size:30px;color:white">Exams</span>
+            </div> 
+            </a>
+        </div>
+        <div class="col col-md-auto d-flex justify-content-center">
+            <a href="<?=PATH?>student/recordShow/<?=$id?>">
+            <div  class="card" style="width: 18rem;height:18rem;background:linear-gradient(to right, #ffff00 12%, #ff6600 70%)">
+            <span style="position: absolute;top:44%;left:40%;font-size:30px;color:white">Files</span>
             </div> 
             </a>
         </div>
